57b307b804f61fda5ac3d0fe32dfd46fbfcd1cab
ntagungira-cmd <ntagungiraali@gmail.com>
2022-03-05 23:22:13 +0200

    footer panel

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,71 +2,88 @@ import React from "react";
 import logo from "../assets/UMWEZI.svg";
 import { Link } from "react-router-dom";
 import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
-import "../styles/footer";
+import "../styles/footer.css";
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="subfooter">
         <div className="description">
-          <img src={logo} alt="logo" width="120" height="120" />
+          <img src={logo} className="img" alt="logo" />
           <p>
             Employees' attitudes at Jonstone Electric Company should be
             improved. The workers do not feel that they are a working team
             instead of just individuals. If people felt they were a part of a
-            team, they would not misuse the tools, or deliberately undermine the
-            work of others.
           </p>
         </div>
         <div className="sitemap">
-          <h4 className="title">Sitemap</h4>
+          <h4>Sitemap</h4>
           <ul>
             <li>
-              <Link to="#">Home page</Link>
+              <Link className="links" to="#">
+                Home page
+              </Link>
             </li>
             <li>
-              <Link to="#">Contact us</Link>
+              <Link className="links" to="#">
+                Contact us
+              </Link>
             </li>
             <li>
-              <Link to="#">How it works</Link>
+              <Link className="links" to="#">
+                How it works
+              </Link>
             </li>
           </ul>
         </div>
         <div className="follow">
-          <h4 className="title">Follow us on</h4>
+          <h4>Follow us on</h4>
           <ul>
             <li>
-              <Link to="#">
+              <Link className="links" to="#">
                 <BsFacebook /> facebook
               </Link>
             </li>
             <li>
-              <Link to="#">
+              <Link className="links" to="#">
                 <BsInstagram /> Insatgram
               </Link>
             </li>
             <li>
-              <Link to="#">
+              <Link className="links" to="#">
                 <BsLinkedin /> Linkedin
               </Link>
             </li>
           </ul>
         </div>
         <div className="contact">
-          <h4 className="title">Contact us</h4>
+          <h4 className="title">Let's get in touch</h4>
           <ul>
             <li>
-              <Link to="#">
+              <Link className="links" to="#">
                 (+250)781351171
               </Link>
             </li>
             <li>
-              <Link to="#">umwezimediaconsult@gmail.com</Link>
+              <Link className="links" to="#">
+                umwezimediaconsult@gmail.com
+              </Link>
             </li>
             <li>
-              <Link to="#">Nyabugogo</Link>
+              <Link className="links" to="#">
+                Nyabugogo
+              </Link>
             </li>
           </ul>
+        </div>
+      </div>
+      <div className="subfooter2">
+        <p>
+          <span>UmweziMediaConsult </span>@ 2022 all Rights Reserved
+        </p>
+        <div>
+          <Link to="#">Privacy Policy</Link>
+          <Link to="#">Terms and Conditions</Link>
         </div>
       </div>
     </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/header";
+import Footer from "../components/footer";
 import "../styles/LandingPage.css";
 import { IoDiscSharp } from "react-icons/io5";
 import { BsTelephone } from "react-icons/bs";
@@ -7,40 +8,41 @@ import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
-    <div className="main">
-      <Header />
-      <div className="content">
-        <div className="text">
-          <h1 className="prom-text">
-            Advertising with <br />
-            <span>Umwezi Media Consult</span>
-            <br />
-            <IoDiscSharp className="disc" />
-            add value to your <br />
-            services.
-          </h1>
-          <p>
-            Best Solution to get your bussiness known to all your clients in an
-            easy and affordable way.
-          </p>
-          <div className="toolTip">
-            <button className="callButton">
-              <BsTelephone />
-            </button>
-            <div className="toolTipText">
-              <p id="toolTipTextOne">Nyabugogo-Kigali</p>
+    <div className="contain">
+      <div className="main">
+        <Header />
+        <div className="content">
+          <div className="text">
+            <h1 className="prom-text">
+              Advertising with <br />
+              <span>Umwezi Media Consult</span>
               <br />
-              <p id="toolTipTextTwo">umwezimediaconsult@gmail.com</p>
+              <IoDiscSharp className="disc" />
+              add value to your <br />
+              services.
+            </h1>
+            <p>
+              Best Solution to get your bussiness known to all your clients in
+              an easy and affordable way.
+            </p>
+            <div className="toolTip">
+              <button className="callButton">
+                <BsTelephone />
+              </button>
+              <div className="toolTipText">
+                <p id="toolTipTextOne">Nyabugogo-Kigali</p>
+                <br />
+                <p id="toolTipTextTwo">umwezimediaconsult@gmail.com</p>
+              </div>
+              <button className="getStarted">
+                <Link to="#" className="login">
+                  Get started today
+                </Link>
+              </button>
             </div>
-            <button className="getStarted">
-              <Link to="#" className="login">
-                Get started today
-              </Link>
-            </button>
           </div>
-        </div>
-        <div className="video">
-          {/* <video width="400" height="350">
+          <div className="video">
+            {/* <video width="400" height="350">
             <source
               src="../../public/resources/React App - Google Chrome 2022-01-19 10-11-52.mp4"
               type="videos/mp4"
@@ -50,8 +52,10 @@ function LandingPage() {
               type="video/webm"
             />
           </video> */}
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
