03e1830503a6a945adfe9af6718586f8afa1ec2a
ntagungira-cmd <ntagungiraali@gmail.com>
2022-03-19 21:41:41 +0200

    debugging video player

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 import VideoPlayer from "react-video-js-player";
 
 function Video() {
-    const source = "../assets/addvertise.mp4";
-    const poster="../assets/poster.png";
-    return (
-        <VideoPlayer
-            controls={true}
-            src={source}
-            poster={poster}
-            width="330"
-            height="260"
-        />
-    )
+  const source = "../assets/addvertise.mp4";
+  const poster = "../assets/poster.png";
+  return (
+    <VideoPlayer
+      controls={true}
+      url={source}
+      poster={poster}
+      width="330"
+      height="260"
+    />
+  );
 }
 
-export default Video
+export default Video;
