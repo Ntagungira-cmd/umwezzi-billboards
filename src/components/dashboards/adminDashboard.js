bea81c87ad2213833e72c86852b068bce04961aa
trezor13 <treasureharden14@gmail.com>
2022-04-12 11:53:45 -0700

    upper navigation bar

--- src/components/dashboards/adminDashboard.js
+++ src/components/dashboards/adminDashboard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import SideNav from './sideNav';
+import Navbar from './navBar';
+import Stats from './stats';
 
 export default function adminDashboard() {
   return (
     <>
         <div>{<SideNav/>}</div>
+        <div>{<Navbar/>}</div>
+        <div>{<Stats/>}</div>
     </>
   )
 }
---
+++ src/components/dashboards/stats.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import './../assets/css/material-dashboard.css?v=3.0.2';
+import './../assets/css/nucleo-icons.css';
+import './../assets/css/nucleo-svg.css';
+import './../assets/js/core/popper.min.js';
+import './../assets/js/core/bootstrap.min.js';
+import './../assets/js/plugins/perfect-scrollbar.min.js';
+import './../assets/js/plugins/smooth-scrollbar.min.js';
+import './../assets/js/plugins/chartjs.min.js';
+import './../assets/js//material-dashboard.min.js?v=3.0.2';
+
+export default function stats() {
+  return (
+    <>
+
+
+    
+
+
+
+
+    </>
+  )
+}
