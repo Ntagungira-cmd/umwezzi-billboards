08cb4eb4a065f26739950bb9bd7a0ce27d845a5f
trezor13 <treasureharden14@gmail.com>
2022-04-12 10:31:16 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import adminDashboard from "./pages/adminDashboard";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <LandingPage />}/>
-        <Route path="/adminDashboard" element={ adminDashboard }/>
-      
+        <Route path="/adminboard" element={ < Admin />}/>    
       </Routes>
     </div>
   );
---
+++ src/pages/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dashboard from  '../components/dashboards/adminDashboard';
+
+export default function Admin() {
+  return (
+    <>
+     {Dashboard}
+    </>
+  )
+}
