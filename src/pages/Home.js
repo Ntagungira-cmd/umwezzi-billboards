4161ab1bfd4fe2b0dd40ab1f85a3923111850f41
ntagungira-cmd <ntagungiraali@gmail.com>
2022-04-23 21:59:08 +0200

    resposiveboards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Board from "../components/board";
+import boards from "../app/boards"
+import "../styles/Board.css";
 
 function Home() {
     return (
         <>
         <div className="HomePage">
             <div className='BoardHolder'>
-                 <Board/>
+                {boards.map((board, index)=>{return <Board board={board} key={index}/>})} 
             </div>
         </div>
 
