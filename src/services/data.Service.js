55b30ada88028f77f515ebba334f501625051fd1
Ntagungira Ali Rashid <83630076+Ntagungira-cmd@users.noreply.github.com>
2022-06-01 21:26:43 +0200

    few bug fixes

--- src/services/data.Service.js
+++ src/services/data.Service.js
@@ -1,7 +1,7 @@
 export default function authHeader() {
   const userData = JSON.parse(localStorage.getItem("userData"));
   if (userData && userData.token) {
-    console.log({ "x-access-token": userData.token });
+    //console.log({ "x-access-token": userData.token });
     return { "X-access-token": userData.token };
   } else {
     return {};
